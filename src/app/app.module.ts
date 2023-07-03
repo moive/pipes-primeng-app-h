0e8b48d969d88aadca6406efb48d69b403a371f7
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-07-02 23:24:54 -0500

    manually change language

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,14 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 
+// Config locales app
+import localeEsPE from '@angular/common/locales/es-PE';
+import localeFr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEsPE);
+registerLocaleData(localeFr);
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
