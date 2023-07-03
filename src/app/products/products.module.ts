496e34eddff6411d366a74d57e98a7a11f456eb7
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-07-02 21:47:11 -0500

    fixes routing

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
+import { BasicPageComponent } from './pages/basic-page/basic-page.component';
+import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
+import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BasicPageComponent,
+    NumbersPageComponent,
+    UncommonPageComponent
+  ],
   imports: [
     CommonModule,
     ProductsRoutingModule
