e3c4fb9eb77cbb4fc2967460f3390c1261a17349
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-07-03 21:26:13 -0500

    pipes: number-decimal currency percent

--- src/app/products/pages/numbers-page/numbers-page.component.ts
+++ src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-numbers-page',
   templateUrl: './numbers-page.component.html',
-  styleUrls: ['./numbers-page.component.scss']
+  styleUrls: ['./numbers-page.component.scss'],
 })
 export class NumbersPageComponent {
-
+  totalSells: number = 2567789.5567;
+  percent: number = 0.4856;
 }
